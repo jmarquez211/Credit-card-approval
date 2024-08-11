21dca67e251d17a53452ba36170f8a812a913971
Márquez <108117107+jmarquez211@users.noreply.github.com>
2024-08-11 20:32:25 +0200

    Update selection.py

--- tabs/selection.py
+++ tabs/selection.py
@@ -17,7 +17,7 @@ def run():
 
     
     # Load datasets 
-    #df_application = pd.read_csv('application_record.csv')
+    df_application = pd.read_csv('application_record.csv')
     df_credit = pd.read_csv('credit_record.csv')
     
     # Process data
